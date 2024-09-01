e44751c85828a264d55777daf2a0670a46a82123
sumondas2001 <sumonchandradas1010@gmail.com>
2024-09-02 01:26:13 +0600

    all complete

--- src/components/PrivetRoutes/PrivetRoutes.jsx
+++ src/components/PrivetRoutes/PrivetRoutes.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 const PrivetRoutes = ({ children }) => {
      const { user, loading } = useContext(AuthContext);
      const location = useLocation();
-     console.log(location.pathname)
+
 
 
      if (loading) {
